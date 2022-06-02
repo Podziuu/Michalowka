42922f83be6033955cce0a4cbd8d02b2b1c9c05a
Bartosz  Podemski <bartekpodemski@gmail.com>
2022-06-02 12:28:21 +0200

    Creating simple navbar

---
+++ src/components/NavBar.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+import logo from "../assets/logo-michalowka.png";
+
+const NavBar = () => {
+  return (
+    <nav className="w-full bg-[#E9C46A] flex items-center text-white px-8 py-2">
+      <a href="#home" className="flex items-center">
+        <img className="w-20" src={logo} />
+      </a>
+      <ul className="flex justify-center w-full text-xl">
+        <li className="mx-4 cursor-pointer">
+          <a href="#onas">O nas</a>
+        </li>
+        <li className="mx-4 cursor-pointer">
+          <a href="#oferta">Oferta</a>
+        </li>
+        <li className="mx-4 cursor-pointer">
+          <a href="#kontakt">Kontakt</a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;
